334d98f4f681869277baf93fe4ba55d713a85793
MaRK0960 <43151702+MaRK0960@users.noreply.github.com>
2019-11-06 09:33:18 +0200

    My files

---
+++ WindowsFormsApplication1/lgnfrm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class lgnfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cnclbtn = new System.Windows.Forms.Button();
+            this.lgnbtn = new System.Windows.Forms.Button();
+            this.usrlbl = new System.Windows.Forms.Label();
+            this.passlbl = new System.Windows.Forms.Label();
+            this.passtxt = new System.Windows.Forms.TextBox();
+            this.usrtxt = new System.Windows.Forms.TextBox();
+            this.sqlCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.SuspendLayout();
+            // 
+            // cnclbtn
+            // 
+            this.cnclbtn.Location = new System.Drawing.Point(148, 175);
+            this.cnclbtn.Name = "cnclbtn";
+            this.cnclbtn.Size = new System.Drawing.Size(75, 23);
+            this.cnclbtn.TabIndex = 4;
+            this.cnclbtn.Text = "Cancel";
+            this.cnclbtn.UseVisualStyleBackColor = true;
+            this.cnclbtn.Click += new System.EventHandler(this.cnclbtn_Click);
+            // 
+            // lgnbtn
+            // 
+            this.lgnbtn.Location = new System.Drawing.Point(27, 175);
+            this.lgnbtn.Name = "lgnbtn";
+            this.lgnbtn.Size = new System.Drawing.Size(75, 23);
+            this.lgnbtn.TabIndex = 3;
+            this.lgnbtn.Text = "Login";
+            this.lgnbtn.UseVisualStyleBackColor = true;
+            this.lgnbtn.Click += new System.EventHandler(this.lgnbtn_Click);
+            // 
+            // usrlbl
+            // 
+            this.usrlbl.AutoSize = true;
+            this.usrlbl.Location = new System.Drawing.Point(24, 72);
+            this.usrlbl.Name = "usrlbl";
+            this.usrlbl.Size = new System.Drawing.Size(55, 13);
+            this.usrlbl.TabIndex = 2;
+            this.usrlbl.Text = "Username";
+            // 
+            // passlbl
+            // 
+            this.passlbl.AutoSize = true;
+            this.passlbl.Location = new System.Drawing.Point(24, 129);
+            this.passlbl.Name = "passlbl";
+            this.passlbl.Size = new System.Drawing.Size(53, 13);
+            this.passlbl.TabIndex = 3;
+            this.passlbl.Text = "Password";
+            this.passlbl.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // passtxt
+            // 
+            this.passtxt.Location = new System.Drawing.Point(106, 126);
+            this.passtxt.Name = "passtxt";
+            this.passtxt.PasswordChar = '*';
+            this.passtxt.Size = new System.Drawing.Size(131, 20);
+            this.passtxt.TabIndex = 2;
+            // 
+            // usrtxt
+            // 
+            this.usrtxt.Location = new System.Drawing.Point(106, 72);
+            this.usrtxt.Name = "usrtxt";
+            this.usrtxt.Size = new System.Drawing.Size(131, 20);
+            this.usrtxt.TabIndex = 1;
+            // 
+            // sqlCommand1
+            // 
+            this.sqlCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=10.10.30.59;User ID=sa;Password=kapsi";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            this.sqlConnection1.InfoMessage += new System.Data.SqlClient.SqlInfoMessageEventHandler(this.sqlConnection1_InfoMessage);
+            // 
+            // lgnfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.usrtxt);
+            this.Controls.Add(this.passtxt);
+            this.Controls.Add(this.passlbl);
+            this.Controls.Add(this.usrlbl);
+            this.Controls.Add(this.lgnbtn);
+            this.Controls.Add(this.cnclbtn);
+            this.Name = "lgnfrm";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cnclbtn;
+        private System.Windows.Forms.Button lgnbtn;
+        private System.Windows.Forms.Label usrlbl;
+        private System.Windows.Forms.Label passlbl;
+        private System.Windows.Forms.TextBox passtxt;
+        private System.Windows.Forms.TextBox usrtxt;
+        private System.Data.SqlClient.SqlCommand sqlCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+    }
+}
+
---
+++ WindowsFormsApplication1/lgnfrm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class lgnfrm : Form
+    {
+        int tries = 0;
+        public lgnfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lgnbtn_Click(object sender, EventArgs e)
+        {
+            if (usrtxt.Text == "" || passtxt.Text == "" )
+            {
+                MessageBox.Show("Plz type a user name and password", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            else
+            {
+                sqlConnection1.Open();
+                sqlCommand1.CommandText = "SELECT [Passowrd]  FROM [Northwind].[dbo].[Employees] WHERE FirstName = '" + usrtxt.Text + "' AND Passowrd = '" + passtxt.Text + "'";
+                string pass = Convert.ToString(sqlCommand1.ExecuteScalar());
+                if (passtxt.Text == pass)
+                {
+                    dtafrm mydta = new dtafrm();
+                    mydta.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    if (++tries < 3)
+                        MessageBox.Show("Wrong Username or Password\n\tPlease try again!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    else
+                    {
+                        MessageBox.Show("Client is locked after Several login attempts!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                    }
+                }
+                sqlConnection1.Close();
+            }
+        }
+
+        private void sqlConnection1_InfoMessage(object sender, System.Data.SqlClient.SqlInfoMessageEventArgs e)
+        {
+
+        }
+
+        private void cnclbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
